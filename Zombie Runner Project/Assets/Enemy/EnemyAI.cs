757385dad01749315d244c585b177ad3246fe7f8
caiogamedesign <caio.gamedesign@gmail.com>
2019-11-13 12:46:08 -0300

    Enabled enemy's chase range gizmo on editor Created patrol state (only rotation)

---
+++ Zombie Runner Project/Assets/Enemy/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    bool IsTargetInRange()
+    {
+        return DistanceToTarget() <= chaseRange;
+    }
+
+    float DistanceToTarget()
+    {
+        return Vector3.Distance(target.position, transform.position);
+    }
+
+    void Update()
+    {
+        if (IsTargetInRange())
+        {
+            Chase();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+
+    private void Patrol()
+    {
+        navMeshAgent.isStopped = true;
+        float yRotation = Mathf.Sin(Time.time/4) * 180 ;
+        transform.localRotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void Chase()
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
