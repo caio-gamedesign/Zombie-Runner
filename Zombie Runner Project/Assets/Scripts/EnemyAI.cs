af4ed7c5689ba886772c321f6430cd3d4da103d3
caiogamedesign <caio.gamedesign@gmail.com>
2019-11-12 21:46:20 -0300

    Enemy chansing player only when in range

--- Zombie Runner Project/Assets/Scripts/EnemyAI.cs
+++ Zombie Runner Project/Assets/Scripts/EnemyAI.cs
@@ -6,17 +6,47 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
-    NavMeshAgent navMeshAgent;
+    [SerializeField] float distanceToChase = 5f;
 
-    // Start is called before the first frame update
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget;
+
+    float catetoX, catetoY, catetoZ;
+    float catetoXSQ, catetoYSQ, catetoZSQ;
+    float hipotenusaSQ;
+    float hipotenusa;
+    float distSQ;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        catetoX = (target.position.x - transform.position.x);
+        catetoY = (target.position.y - transform.position.y);
+        catetoZ = (target.position.z - transform.position.z);
+
+        catetoXSQ = catetoX * catetoX;
+        catetoYSQ = catetoY * catetoY;
+        catetoZSQ = catetoZ * catetoZ;
+
+        hipotenusaSQ = catetoXSQ + catetoYSQ + catetoZSQ;
+        hipotenusa = Mathf.Sqrt(hipotenusaSQ);
+
+        distSQ = distanceToChase * distanceToChase;
+
+        if (distanceToTarget <= distanceToChase)
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.SetDestination(target.position);
+        }
+        else
+        {
+            navMeshAgent.isStopped = true;
+        }
     }
 }
