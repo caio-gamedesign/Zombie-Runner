50be6f4dcf57d2507b42068d0a7919c97cf69f51
caiogamedesign <caio.gamedesign@gmail.com>
2019-11-13 20:11:38 -0300

    Player shooting Enemy Enemy dying

---
+++ Zombie Runner Project/Assets/Asset Packs/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [Range(1,100)][SerializeField] int damage = 25;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name + " hitted");
+
+            if (hit.transform.CompareTag("Enemy"))
+            {  
+                hit.transform.GetComponent<EnemyHealth>().Damage(damage);
+            }
+        }
+    }
+}
---
+++ Zombie Runner Project/Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float healthAmount = 100f;
+
+    public void Damage(float damageAmount)
+    {
+        Debug.Log(damageAmount);
+        if (damageAmount > 0)
+        {
+            healthAmount -= damageAmount;
+        }
+
+        if (healthAmount <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
